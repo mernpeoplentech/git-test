b5bf898f705eb0686ec7d27fc52a7b57f65d810e
= <=>
2024-09-07 12:27:30 +0600

    kajfj

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,49 @@
 import Blog from "./components/Blog";
 
 function App() {
+
+  const blogs = [
+    {
+      title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ipsam.',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora magnam officiis explicabo soluta, quibusdam aperiam nihil voluptates adipisci esse voluptate quae? Repellendus, excepturi sequi! Doloremque explicabo quasi doloribus deleniti dolore consequuntur, aspernatur voluptate ut sunt blanditiis suscipit accusantium aut tempore.'
+    },
+    {
+      title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ipsam.',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora magnam officiis explicabo soluta, quibusdam aperiam nihil voluptates adipisci esse voluptate quae? Repellendus, excepturi sequi! Doloremque explicabo quasi doloribus deleniti dolore consequuntur, aspernatur voluptate ut sunt blanditiis suscipit accusantium aut tempore.'
+    },
+    {
+      title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ipsam.',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora magnam officiis explicabo soluta, quibusdam aperiam nihil voluptates adipisci esse voluptate quae? Repellendus, excepturi sequi! Doloremque explicabo quasi doloribus deleniti dolore consequuntur, aspernatur voluptate ut sunt blanditiis suscipit accusantium aut tempore.'
+    },
+    {
+      title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ipsam.',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora magnam officiis explicabo soluta, quibusdam aperiam nihil voluptates adipisci esse voluptate quae? Repellendus, excepturi sequi! Doloremque explicabo quasi doloribus deleniti dolore consequuntur, aspernatur voluptate ut sunt blanditiis suscipit accusantium aut tempore.'
+    },
+    {
+      title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ipsam.',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora magnam officiis explicabo soluta, quibusdam aperiam nihil voluptates adipisci esse voluptate quae? Repellendus, excepturi sequi! Doloremque explicabo quasi doloribus deleniti dolore consequuntur, aspernatur voluptate ut sunt blanditiis suscipit accusantium aut tempore.'
+    },
+    {
+      title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ipsam.',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora magnam officiis explicabo soluta, quibusdam aperiam nihil voluptates adipisci esse voluptate quae? Repellendus, excepturi sequi! Doloremque explicabo quasi doloribus deleniti dolore consequuntur, aspernatur voluptate ut sunt blanditiis suscipit accusantium aut tempore.'
+    }
+  ]
+
   return (
     <main style={{ padding: 10 }}>
-      <h1>Joynta kumar barman</h1>
-
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus
-        aspernatur ratione, amet tenetur, dolore perferendis illum ad at
-        aliquam, quis qui sapiente! Minus, est necessitatibus!
-      </p>
-      <div>
-        <h4>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla,
-          recusandae.
+      
+      <div >
+        <h4 style={{display: 'flex', justifyContent: 'center', fontSize: '36px', color: 'darkblue' }}>
+          My Blogs
         </h4>
+      <div style={{display: 'grid',   gap: '12px'}}>
+        {
+          blogs.map(item => (
+            <Blog blog={item}/>
+          ))
+        }
       </div>
-      <Blog />
-      <h2>Conclusion</h2>
+      </div>
     </main>
   );
 }
--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 
-const Blog = () => {
+const Blog = ({ blog }) => {
   return (
     <div className="blog">
-      <h3>Joynta's Blog</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque dicta
-        rem, similique vel ex magnam ea eligendi neque eum optio qui animi quam
-        accusamus, aperiam harum numquam temporibus earum quibusdam voluptate.
-        Consequatur facilis reprehenderit praesentium quos quidem sit assumenda
-        enim earum, temporibus reiciendis odio sapiente autem expedita maxime,
-        aperiam velit!
-      </p>
+      <h3 style={{ cursor: "pointer", color: "darkorange" }}>{blog?.title}</h3>
+      <p>{blog?.description}</p>
     </div>
   );
 };
