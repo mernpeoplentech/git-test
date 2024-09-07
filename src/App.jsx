82c80dd9942939f9778812ece0bf55c39ed8339c
Apurbo <apurbo501893@gmail.com>
2024-09-07 12:26:33 +0600

    style done

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import Blog from "./components/Blog";
 export default function App() {
   return (
     <main style={{ padding: 10 }}>
-      <div>
-        <h4>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla,
-          recusandae.
-        </h4>
+      <div style={{ textAlign: "center" }}>
+        <div>
+          <h1 style={{ color: "white", background: "orange" }}>Apurbo Tudu</h1>
+        </div>
+        <Blog />
+        <button style={{ background: "orange", color: "white" }}>
+          Conclusion
+        </button>
       </div>
-      <Blog />
-      <h2>Conclusion</h2>
     </main>
   );
 }
--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -3,14 +3,22 @@ import React from "react";
 const Blog = () => {
   return (
     <div className="blog">
-      <h3>Apurbo Blog</h3>
+      <h3 style={{ border: "2px", borderColor: "green" }}>Apurbo Blog</h3>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque dicta
-        rem, similique vel ex magnam ea eligendi neque eum optio qui animi quam
-        accusamus, aperiam harum numquam temporibus earum quibusdam voluptate.
-        Consequatur facilis reprehenderit praesentium quos quidem sit assumenda
-        enim earum, temporibus reiciendis odio sapiente autem expedita maxime,
-        aperiam velit!
+        Welcome to Apurbo Blogs, where visual storytelling meets creative
+        expression. Our blog features a curated collection of stunning
+        photographs that capture moments, landscapes, and cultures from around
+        the world. Each post is a blend of striking imagery and insightful
+        commentary, designed to transport you to diverse settings and offer
+        fresh perspectives. From breathtaking natural wonders and vibrant urban
+        scenes to intimate portraits and abstract compositions, our photos are
+        chosen to inspire and evoke emotions. We delve into the stories behind
+        each image, sharing the experiences of the photographers and the
+        contexts that shaped their work. Whether you’re a photography enthusiast
+        or simply love to explore through images, Apurbo Blogs offers a visual
+        feast that celebrates the art of photography and the beauty of the
+        world. Join us on this visual journey, where every photo tells a story
+        and every post invites you to see the world through a different lens.
       </p>
     </div>
   );
