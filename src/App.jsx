d59e2af00dafd9aa3c5af9aeb4ab75252c13f252
= <=>
2024-09-07 11:43:01 +0600

    conflict 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Blog from "./components/Blog";
 function App() {
   return (
     <main style={{ padding: 10 }}>
-      <h1>Noor Mohammad</h1>
-      <h1>My name is joynta</h1>
+      <h1>Joynta kumar barman</h1>
+
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus
         aspernatur ratione, amet tenetur, dolore perferendis illum ad at
