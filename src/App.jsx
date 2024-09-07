97da9fce8d94ad5078275e19d3fa3ab1e4de8acc
= <=>
2024-09-07 11:14:02 +0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <main style={{ padding: 10 }}>
       <h1>Noor Mohammad</h1>
+      <h2>Joynta kumar barman</h2>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus
         aspernatur ratione, amet tenetur, dolore perferendis illum ad at
