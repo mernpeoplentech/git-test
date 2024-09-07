51182f31d4866a2101fe9b5fa95af99d80426865
Apurbo <apurbo501893@gmail.com>
2024-09-07 12:46:12 +0600

    todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
-import Blog from "./components/Blog";
-import Navbar from "./components/Navbar";
-
 export default function App() {
   return (
-    <main style={{ padding: 10 }}>
-      <Navbar />
-      <div>
-        <h2>Welcome To Dummy Blog</h2>
-      </div>
-      <div>
-        <h3 className="tech">Latest Technologies</h3>
-      </div>
-      <Blog />
-      <footer>All rights reserved @ dummy blog</footer>
+    <main>
+      <p>My todo app will go here</p>
     </main>
   );
 }
