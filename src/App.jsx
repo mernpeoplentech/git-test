3badd39c505d7e4d84a7b4b9ad7ea5b87d7e9c42
mernpeoplentech <167546607+mernpeoplentech@users.noreply.github.com>
2024-09-07 12:02:23 +0600

    updated from main

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import Blog from "./components/Blog";
 function App() {
   return (
     <main style={{ padding: 10 }}>
-      <h1>Noor Mohammad</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus
-        aspernatur ratione, amet tenetur, dolore perferendis illum ad at
-        aliquam, quis qui sapiente! Minus, est necessitatibus!
-      </p>
+      <div>
+        <h4>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla,
+          recusandae.
+        </h4>
+      </div>
       <Blog />
       <h2>Conclusion</h2>
     </main>
