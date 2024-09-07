b147dd7fc64a8f1c999a73405387f8d2ebef7d8e
Md Sujon Islam <iamsujonislam06@gmail.com>
2024-09-07 22:54:03 +0600

    tailwind add

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <main>
-      <div>My Todo app will go here</div>
+      <div className="text-center bg-purple-500">My Todo app will go here</div>
     </main>
   );
 }
