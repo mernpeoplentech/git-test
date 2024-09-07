e5a38ed9f58d6bf9a5bc920b67f7ea0ee9d58670
= <=>
2024-09-07 12:46:07 +0600

    new todo project

--- src/App.jsx
+++ src/App.jsx
@@ -32,15 +32,7 @@ function App() {
 
   return (
     <main style={{ padding: 10 }}>
-      <Navbar />
-      <div>
-        <h2>Welcome To Dummy Blog</h2>
-      </div>
-      <div>
-        <h3 className="tech">Latest Technologies</h3>
-      </div>
-      <Blog />
-      <footer>All rights reserved @ dummy blog</footer>
+      This is my todo app here.
     </main>
   );
 }
