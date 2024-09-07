9916eb6f7ce260498e076a8bbd74f9e37026239b
mahirm123 <mahirm8123@gmail.com>
2024-09-07 11:18:10 +0600

    something updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 function App() {
   return (
     <main style={{ padding: 10 }}>
-      <h1 className="font-semibold" style={{color:"blue"}}>Noor Mohammad</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus
-        aspernatur ratione, amet tenetur, dolore perferendis illum ad at
-        aliquam, quis qui sapiente! Minus, est necessitatibus!
-      </p>
-      <p>amet consectetur adipisicing elit. Sequi rerum quaerat optio sapiente maiores officiis explicabo saepe rem veritatis praesentium.</p>
-      <h2>Conclusion</h2>
+      <h1 className="font-semibold" style={{ color: "blue" }}>
+        Noor Mohammad
+      </h1>
+      <h3 className="title">Software Engineer</h3>
+      <div className="des">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Necessitatibus aspernatur ratione, amet tenetur, dolore perferendis
+          illum ad at aliquam, quis qui sapiente! Minus, est necessitatibus!
+        </p>
+        <p>
+          amet consectetur adipisicing elit. Sequi rerum quaerat optio sapiente
+          maiores officiis explicabo saepe rem veritatis praesentium.
+        </p>
+      </div>
     </main>
   );
 }
