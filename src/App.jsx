47ac4dda268c7bd0b406453d4c289d80436472f2
M-Shourav <muhammadshourav625@gmail.com>
2024-09-07 12:31:38 +0600

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Blog from "./components/Blog";
+import Header from "./components/Header";
 
 function App() {
   return (
     <main className="bodyStyle">
+      <Header />
       <div className="title">
         <img src="./src/assets/logoipsum-327.svg" alt="" />
         <h1>Muhammad Shourav</h1>
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <ul className="ul">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
