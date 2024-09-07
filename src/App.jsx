a8b04bde8664b6387025a1083c300e2a70baa1e5
Dev-shahinalom0 <smmshahinalom@gmail.com>
2024-09-07 12:45:20 +0600

    todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import Blog from "./components/Blog";
-import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
-    <main style={{ padding: 10 }}>
-      <Navbar />
-      <div>
-        <h2>Welcome To Dummy Blog</h2>
-      </div>
-      <div>
-        <h3 className="tech">Latest Technologies</h3>
-      </div>
-      <Blog />
-      <footer>All rights reserved @ dummy blog</footer>
+    <main>
+<p>my todo app</p>
     </main>
   );
 }
